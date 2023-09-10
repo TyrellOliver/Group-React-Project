da9f564c646f95e21cc293edbee6c48fb14ac2d0
JackieC1993 <jackiecheung@pursuit.org>
2023-09-10 12:58:54 -0400

    edit the sandwich menu, add my personal sandwich

---
+++ src/Components/Menu.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SandwichMenu(){
+    const sandwiches = [
+    { name: 'Cheung Sandwich', ingredients: ['Pastrami','Feta Cheese', 'Provolone Cheese', 'Avocado','Pickles','Banana Pepper', 'Mixed Green','Pesto','Honey Mustard']}
+    ];
+}
