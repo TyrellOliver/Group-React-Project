35377edcfc47a3ef5dea3ab0d1dfe0fbce6734a8
Rich Boyce <122512687+rboyce212@users.noreply.github.com>
2023-09-11 12:59:13 -0400

    adjusted data table

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import { useState } from "react";
 import "./App.css";
 
-import Home from "./Components/Home"
-import About from "./Components/About"
+import Home from "./Components/Home";
+import About from "./Components/About";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Router>
       {/* <NavBar /> */}
