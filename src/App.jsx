2f31c96ded5b4981dc4e1821083a1698366e887e
JackieC1993 <jackiecheung@pursuit.org>
2023-09-12 12:37:53 -0400

    revised the app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
+import React from 'react';
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
 import Home from "./Components/Home"
 import About from "./Components/About"
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <Router>
-      {/* <NavBar /> */}
+    <Router>    
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,5 @@
 import NavBar from "./NavBar";
+import "./Home.css";
 
 const Home = () => {
   return (
