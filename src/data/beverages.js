23ad07432eb55b6b2a2bf4d82e54928d4daf617d
Rich Boyce <122512687+rboyce212@users.noreply.github.com>
2023-09-17 13:31:37 -0400

    regained functionality of build your own and added images

--- src/data/beverages.js
+++ src/data/beverages.js
@@ -2,37 +2,37 @@ const beverages = [
   {
     id: "bev-01",
     name: "Bottled Coca-Cola (20 oz.)",
-    price: 1.99
+    price: 199
   },
   {
     id: "bev-02",
     name: "Bottled Diet Coke (20 oz.)",
-    price: 1.99
+    price: 199
   },
   {
     id: "bev-03",
     name: "Bottled Sprite (20 oz.)",
-    price: 1.99
+    price: 199
   },
   {
     id: "bev-04",
     name: "Bottled Diet Dr Pepper (20 oz.)",
-    price: 1.99
+    price: 199
   },
   {
     id: "bev-05",
     name: "Bottled Ginger Ale (20 oz.)",
-    price: 1.99
+    price: 199
   },
   {
     id: "bev-06",
     name: "Bottled Orange Fanta (20 oz.)",
-    price: 1.99
+    price: 199
   },
   {
     id: "bev-07",
     name: "Dasani Bottled Water (20 oz.)",
-    price: 1.25
+    price: 125
   }
 ];
 export default beverages;
